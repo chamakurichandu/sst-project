a499bcae3c4e7bd6cf0eea8698838b66a37e87c2
chamakurichandu <chandra.ch@abitechnologies.in>
2021-04-02 16:41:08 +0530

    testing,acceptance,handover UI completed

--- src/components/workAcceptance.jsx
+++ src/components/workAcceptance.jsx
@@ -11,7 +11,6 @@ import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import LinearProgress from '@material-ui/core/LinearProgress';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
 import config from "../config.json";
@@ -20,11 +19,8 @@ import MuiAlert from '@material-ui/lab/Alert';
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import ProcurementImage from '../assets/svg/ss/commercial-2.svg';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteImage from '@material-ui/icons/Delete';
-import SelectItem from './selectItem';
-import cloneDeep from 'lodash/cloneDeep';
 import TextField from '@material-ui/core/TextField';
+import AddMaterialIndent from './addMaterialIndent';
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -40,11 +36,11 @@ function EnhancedTableHead(props) {
     { id: 'name', numeric: false, disablePadding: false, label: 'Name' },
     { id: 'description', numeric: false, disablePadding: false, label: 'Description' },
     { id: 'uom', numeric: false, disablePadding: false, label: 'UOM' },
-    { id: 'qty', numeric: false, disablePadding: false, label: 'Quantity' },
-    { id: 'actions', numeric: false, disablePadding: false, label: 'Actions' }
+    { id: 'testing_qty', numeric: false, disablePadding: false, label: 'Testing Quantity' },
+    { id: 'acceptance_qty', numeric: false, disablePadding: false, label: 'Acceptance Quantity' }
   ];
 
-  const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+  const { classes, order, orderBy, numSelected, rowCount, onRequestSort } = props;
   const createSortHandler = (property) => (event) => {
     onRequestSort(event, property);
   };
@@ -81,13 +77,12 @@ EnhancedTableHead.propTypes = {
   classes: PropTypes.object.isRequired,
   numSelected: PropTypes.number.isRequired,
   onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
   order: PropTypes.oneOf(['asc', 'desc']).isRequired,
   orderBy: PropTypes.string.isRequired,
   rowCount: PropTypes.number.isRequired,
 };
 
-export default function WorkInstallation(props) {
+export default function WorkAcceptance(props) {
 
   // console.log("props: " + props);
 
@@ -224,15 +219,108 @@ export default function WorkInstallation(props) {
   const [order, setOrder] = React.useState('asc');
   const [orderBy, setOrderBy] = React.useState('calories');
   const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense] = React.useState(true);
+
   const [showError, setShowError] = React.useState(false);
   const [errorMessage, setErrorMessage] = React.useState(null);
-  const [rows, setRows] = React.useState([]);
+  const [indents, setIndents] = React.useState([]);
   const [showSaved, setShowSaved] = React.useState(false);
+  const [showAddMaterialIndent, setShowAddMaterialIndent] = React.useState(false);
+  const [materialIndents, setMaterialIndents] = React.useState([]);
+  const [warehouses, setWarehouses] = React.useState([]);
+  const [uoms, set_uoms] = React.useState([]);
+  const [work, set_work] = React.useState(null);
+  const [items, set_items] = React.useState([]);
+  const [items_error, set_items_error] = React.useState(null);
+  const [editMode, setEditMode] = React.useState(false);
 
   const [showBackDrop, setShowBackDrop] = React.useState(false);
 
+  async function getUOMList() {
+    try {
+      setShowBackDrop(true);
+      let url = config["baseurl"] + "/api/uom/list";
+      axios.defaults.headers.common['authToken'] = window.localStorage.getItem("authToken");
+      const { data } = await axios.get(url);
+      set_uoms(data.list);
+
+      setShowBackDrop(false);
+    }
+    catch (e) {
+      if (e.response) {
+        console.log("Error in getting UOMs list");
+        setErrorMessage(e.response.data.message);
+      }
+      else {
+        console.log("Error in getting UOMs list");
+        setErrorMessage("Error in UOMs list");
+      }
+      setShowError(true);
+      setShowBackDrop(false);
+    }
+  }
+
+  async function getWorkDetails() {
+    try {
+      setShowBackDrop(true);
+      let url = config["baseurl"] + "/api/work/details?id=" + props.projectWork.work._id;
+      axios.defaults.headers.common['authToken'] = window.localStorage.getItem("authToken");
+
+      const { data } = await axios.get(url);
+      set_work(data);
+
+      let newItems = [];
+      if (data.work.survey_materials) {
+        for (let i = 0; i < data.work.survey_materials.length; ++i) {
+          let item = getItem(data.work.survey_materials[i].item);
+          item.qty = data.work.survey_materials[i].qty ? parseInt(data.work.survey_materials[i].qty) : 0;
+          item.install_qty = data.work.survey_materials[i].install_qty ? parseInt(data.work.survey_materials[i].install_qty) : 0;
+          newItems.push(item);
+        }
+      }
+      set_items(newItems);
+
+      setShowBackDrop(false);
+    }
+    catch (e) {
+      if (e.response) {
+        console.log("Error in getting work details");
+        setErrorMessage(e.response.data.message);
+      }
+      else {
+        console.log("Error in getting work details");
+        setErrorMessage("Error in getting list");
+      }
+      setShowError(true);
+      setShowBackDrop(false);
+    }
+  }
+
   useEffect(() => {
+    console.log("props.workData: ", props.workData);
+    getUOMList();
   }, []);
+
+  useEffect(() => {
+    if (props.allItems.length > 0)
+      getWorkDetails();
+  }, [props.allItems]);
+
+  
+  const getItem = (id) => {
+    for (let i = 0; i < props.allItems.length; ++i) {
+      if (props.allItems[i]._id === id)
+        return props.allItems[i];
+    }
+    return null;
+  };
+
+  const set_item_qty_for = (value, index) => {
+    let newItems = [...items];
+    newItems[index].install_qty = value;
+    set_items(newItems);
+  };
 
   const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
@@ -260,7 +348,7 @@ export default function WorkInstallation(props) {
       let url = config["baseurl"] + "/api/work/completestep";
 
       let postObj = {};
-      postObj["step"] = "acceptance";
+      postObj["step"] = "installation";
 
       let updateObj = { _id: props.projectWork.work._id, updateParams: postObj };
 
@@ -288,21 +376,180 @@ export default function WorkInstallation(props) {
     }
   };
 
+  const handleAddMaterialIndent = () => {
+    setShowAddMaterialIndent(true);
+  };
+
+  const closeAddMaterialIndentDialog = () => {
+    setShowAddMaterialIndent(false);
+    // getMaterialIndentList();
+  };
+
+  const handleSelectAllClick = (event) => {
+    // if (event.target.checked) {
+    //   const newSelecteds = rows.map((n) => n.name);
+    //   setSelected(newSelecteds);
+    //   return;
+    // }
+    // setSelected([]);
+  };
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const gotoIndentDetails = () => {
+
+  };
+
+  const getWarehouseName = (id) => {
+    for (let i = 0; i < warehouses.length; ++i) {
+      if (warehouses[i]._id === id) {
+        return warehouses[i].name;
+      }
+    }
+    return "Unknown";
+  };
+
+  const getuomFor = (value) => {
+    for (let i = 0; i < uoms.length; ++i) {
+      if (value === uoms[i]._id)
+        return uoms[i].name;
+    }
+    return value;
+  }
+
+  const handleSave = async () => {
+    if (!editMode) {
+      setEditMode(true);
+      return;
+    }
+
+    let errorOccured = false;
+    for (let i = 0; i < items.length; ++i) {
+      if (parseInt(items[i].qty) === 0) {
+        setErrorMessage("qty cannot be zero");
+        setShowError(true);
+        errorOccured = true;
+        break;
+      }
+    }
+
+    if (errorOccured)
+      return;
+
+    try {
+      setShowBackDrop(true);
+      let url = config["baseurl"] + "/api/work/setinstallationmaterials";
+
+      let postObj = {};
+      postObj["items"] = [];
+      for (let i = 0; i < items.length; ++i) {
+        if (parseInt(items[i].install_qty > parseInt(items[i].qty))) {
+          setErrorMessage("installation qty cannot be greater than survey qty");
+          setShowError(true);
+          return;
+        }
+
+        postObj["items"].push({ item: items[i]._id, install_qty: parseInt(items[i].install_qty) });
+      }
+
+      console.log("postObj: ", postObj);
+      let updateObj = { _id: props.projectWork.work._id, updateParams: postObj };
+
+      axios.defaults.headers.common['authToken'] = window.localStorage.getItem("authToken");
+
+      const response = await axios.patch(url, updateObj);
+      console.log("successfully Saved");
+      setShowBackDrop(false);
+      setShowSaved(true);
+
+      setEditMode(false);
+
+      getWorkDetails();
+    }
+    catch (e) {
+      console.log("5");
+      if (e.response) {
+        console.log("Error in creating");
+        setErrorMessage(e.response.data["message"]);
+      }
+      else {
+        console.log("Error in creating");
+        setErrorMessage("Error in creating: ", e.message);
+      }
+      setShowError(true);
+      setShowBackDrop(false);
+    }
+  };
+
+  const handleCancel = () => {
+    setEditMode(false);
+  };
+
   return (
     <div className={clsx(classes.root)}>
       <div className={classes.paper}>
-
         <Paper className={classes.grid}>
           <Grid container spacing={2}>
             <Grid item className={classes.totalAttendes}>
+              <img src={ProcurementImage} width='25' alt="" />
+              <h1 className={classes.h1}>{items.length}</h1>
+              <span>{"Materials in Acceptance"}</span>
             </Grid>
             <Grid item className={classes.addButton}>
-              <Button onClick={() => handleComplete()} style={{ background: "#314293", color: "#FFFFFF" }} variant="contained" className={classes.button}>{"Complete Acceptance"}</Button>
+              {!editMode && <Button onClick={() => handleComplete()} style={{ background: "#314293", color: "#FFFFFF", marginLeft: 5 }} variant="contained" className={classes.button}>{"Complete Acceptance"}</Button>}
+              <Button onClick={() => handleSave()} style={{ marginLeft: 10, background: "#314293", color: "#FFFFFF" }} variant="contained" className={classes.button}>{editMode ? "Save Acceptance" : "Edit Acceptance"}</Button>
+              {editMode && <Button onClick={() => handleCancel()} style={{ marginLeft: 20, background: "#314293", color: "#FFFFFF" }} variant="contained" className={classes.button}>{"Cancel"}</Button>}
             </Grid>
           </Grid>
         </Paper>
-      </div>
 
+        <Paper className={classes.grid}>
+          <TableContainer>
+            <Table
+              className={classes.table}
+              aria-labelledby="tableTitle"
+              size={dense ? 'small' : 'medium'}
+              aria-label="enhanced table"
+            >
+              <EnhancedTableHead
+                classes={classes}
+                numSelected={selected.length}
+                order={order}
+                orderBy={orderBy}
+                onSelectAllClick={handleSelectAllClick}
+                onRequestSort={handleRequestSort}
+                rowCount={items.length}
+              />
+              <TableBody>
+                {items.map((row, index) => {
+                  const labelId = `enhanced-table-checkbox-${index}`;
+                  return (
+                    <TableRow hover tabIndex={-1} key={labelId}>
+                      <TableCell align={dir === 'rtl' ? 'right' : 'left'} component="th" id={labelId} scope="row" padding="none">{(index + 1)}</TableCell>
+                      <TableCell align={dir === 'rtl' ? 'right' : 'left'} >{row.code}</TableCell>
+                      <TableCell align={dir === 'rtl' ? 'right' : 'left'} >{row.name}</TableCell>
+                      <TableCell align={dir === 'rtl' ? 'right' : 'left'} >{row.description}</TableCell>
+                      <TableCell align={dir === 'rtl' ? 'right' : 'left'} >{getuomFor(row.uomId)}</TableCell>
+                      <TableCell align={dir === 'rtl' ? 'right' : 'left'} >
+                        <TextField size="small" id={"formControl_qty_" + index} type="number" value={row.qty} variant="outlined" disabled />
+                      </TableCell>
+                      <TableCell align={dir === 'rtl' ? 'right' : 'left'} >
+                        <TextField size="small" id={"formControl_installation_qty_" + index} type="number" value={row.install_qty}
+                          variant="outlined" disabled={!editMode} onChange={(event) => { set_item_qty_for(event.target.value, index) }} />
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>        
+      </div>
+      {showAddMaterialIndent && <AddMaterialIndent workData={props.workData} indents={indents} closeAction={closeAddMaterialIndentDialog} materialIndents={materialIndents} warehouses={warehouses} {...props} />}
       <Snackbar open={showError} autoHideDuration={6000} onClose={handleClose}>
         <Alert onClose={handleClose} severity="error">
           {errorMessage}
